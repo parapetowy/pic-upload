007c94574d8485778c321d7945da8d09a9f90092
Hubert Hadyński <hubert.hadynski@gmail.com>
2018-09-17 13:46:52 +0200

    fixes

--- modules/handlers.js
+++ modules/handlers.js
@@ -5,21 +5,33 @@ exports.upload = function(request, response) {
     console.log("Rozpoczynam obsługę żądania upload.");
     var form = new formidable.IncomingForm();
     form.parse(request, function(error, fields, files) {
-        fs.renameSync(files.upload.path, "test.png");
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write("received image:<br/>");
-        response.write("<img src='/show' />");
-        response.end();
+        if (error) {
+            console.log('Error - ' + error)
+        } else {
+            fs.renameSync(files.upload.path, "test.png");
+            response.writeHead(200, {
+                "Content-Type": "text/html"
+            });
+            response.write("received image:<br/>");
+            response.write("<img src='/show' />");
+            response.end();
+        }
     });
 }
 
 exports.welcome = function(request, response) {
     console.log("Rozpoczynam obsługę żądania welcome.");
     fs.readFile('templates/start.html', function(err, html) {
-            response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+        if (err) {
+            console.log('Error - ' + err)
+        } else {
+            response.writeHead(200, {
+                "Content-Type": "text/html; charset=utf-8"
+            });
             response.write(html);
             response.end();
-        });
+        }
+    });
 }
 
 exports.error = function(request, response) {
@@ -30,7 +42,9 @@ exports.error = function(request, response) {
 
 exports.show = function(request, response) {
     fs.readFile("test.png", "binary", function(error, file) {
-        response.writeHead(200, {"Content-Type": "image/png"});
+        response.writeHead(200, {
+            "Content-Type": "image/png"
+        });
         response.write(file, "binary");
         response.end();
     });
